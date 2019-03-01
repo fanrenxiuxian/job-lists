1ed80d28da5ea610370d5b1efc2dd03a18148660
fanrenxiuxian <51xiuxian@sina.com>
2019-03-01 12:49:21 +0800

    must login before CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
 
   def index
     @jobs = Job.find_each(batch_size: 100)
