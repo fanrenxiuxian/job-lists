f5d97a49e1cb93462dce47844e5fad177995176b
fanrenxiuxian <51xiuxian@sina.com>
2019-03-01 12:25:01 +0800

    user can delete a job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -32,6 +32,13 @@ class JobsController < ApplicationController
     end
   end
 
+  def destroy
+    @job = Job.find params[:id]
+    @job.destroy
+    redirect_to jobs_path
+    flash[:alert] = "删除成功"
+  end
+
   private
 
   def job_params
