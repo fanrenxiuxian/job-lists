68e6feaad56f0e8c2a21aa4b343948f652347960
fanrenxiuxian <51xiuxian@sina.com>
2019-03-05 16:17:53 +0800

    implement admin::jobs#edit

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -4,4 +4,23 @@ class Admin::JobsController < ApplicationController
     @jobs = Job.all
   end
 
+  def edit
+    @job = Job.find params[:id]
+  end
+
+  def update
+    @job = Job.find params[:id]
+
+    if @job.update job_params
+      redirect_to admin_jobs_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
