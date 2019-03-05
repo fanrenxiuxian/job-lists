1d479becf2d2974e3f216b3ddc440fd7af7fc400
fanrenxiuxian <51xiuxian@sina.com>
2019-03-05 20:56:30 +0800

    add more details to job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_090044) do
+ActiveRecord::Schema.define(version: 2019_03_05_125330) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2019_03_05_090044) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_hidden", default: false
+    t.integer "wage_upper_bound"
+    t.integer "wage_lower_bound"
+    t.string "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
